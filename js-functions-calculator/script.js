a48e773f24a83096dfc30de01388a01a4ae66abd
Asma Shahzadi <asmashahzadi764@gmail.com>
2026-05-25 14:27:06 +0500

    Add basic arithmetic functions in script.js

---
+++ js-functions-calculator/script.js
@@ -0,0 +1,24 @@
+//Add Function
+function Add(a,b){
+    return a + b;
+}
+
+//Subtract Function (Arrow)
+const Subtract =(a,b) =>{
+     return a - b;
+}
+
+//Multiply Function
+function Multiply(a,b){
+    return a * b;
+}
+
+//Divide Function (Arrow)
+const Divide = (a, b) => {
+    return a/b;
+}
+
+console.log("Add:", Add(10,5));
+console.log("Subtract:", Subtract(10,5));
+console.log("Multiply:",Multiply(10,5));
+console.log("Divide:",Divide(10,5));
