1bddecf8d3e3486a7d811a2559175eea59e61964
Asma Shahzadi <asmashahzadi764@gmail.com>
2026-06-02 19:32:53 +0500

    Create script.js

---
+++ js-dom-innerhtml-practice/script.js
@@ -0,0 +1,10 @@
+const heading = document.querySelector("#heading");
+
+const changeBtn = document.querySelector("#changeBtn");
+
+changeBtn.addEventListener("click", () => {
+
+    heading.innerHTML =
+        "Welcome <strong>Asma Shahzadi</strong>";
+
+});
